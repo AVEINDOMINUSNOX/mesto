40e91952df8ab77b75c797fa048d3fc2e8973bc0
Shinkarenko Anton <AVEINDOMINUSNOX@yandex.ru>
2022-12-07 20:55:45 +0500

    fix: исправление после первого ревью

--- scripts/script.js
+++ scripts/script.js
@@ -7,23 +7,45 @@ const popupCloseButton = popup.querySelector('.popup__close-button');
 
 let nameProfile = profile.querySelector('.profile__user-name');
 let jobProfile = profile.querySelector('.profile__user-specialization');
-let formElement = popup.querySelector('.popup__input-container');
+/*let formElement = popup.querySelector('.popup__input-container');
+ Раз мы уже указали имя в html, здесь уже можно не указывать?
+ код работает и так, и так*/
 let nameInput = popup.querySelector('.popup__input_name');
 let jobInput = popup.querySelector('.popup__input_specialization');
+let itemName = pageContainer.querySelector('.itemName');
 
-/*открытие popup*/
-function opened() {
-  popup.classList.add('popup_opened');
+/*Я уже не могу придумать как иначе реализовать изменение названия последней карточке при
+изменении ширины экрана)) */
+function change(){
+ if (document.documentElement.clientWidth > 620){
+  document.getElementById("itemName").textContent = "Карачаево-Черкесия";
+}else{
+  document.getElementById("itemName").textContent = "Карачаевск";
+}
+ }
+
+function openPopup() {
+  popup.classList.add('popup_open');
   nameInput.value = nameProfile.textContent;
   jobInput.value = jobProfile.textContent;
 }
-profileEditButton.addEventListener('click', opened);
 
-/*звкрытие popup*/
-function closed() {
-  popup.classList.remove('popup_opened');
+function closePopup() {
+  popup.classList.remove('popup_open');
 }
-popupCloseButton.addEventListener('click', closed);
+
+function savePopup(evt) {
+  evt.preventDefault();
+  nameProfile.textContent = nameInput.value;
+  jobProfile.textContent = jobInput.value;
+  closePopup();
+}
+
+profileEditButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
+formElement.addEventListener('submit', savePopup);
+window.addEventListener("resize", change);
+
 
 /*Реализовал закрытие popup нажатием на любую область, кроме как popup__container, но потому увидел,
 что в задании такого не было и решил не включать данную фичу, а удалять код жалко
@@ -33,14 +55,6 @@ popup.addEventListener('click', function(closedPopup){
   }
 });
 */
-/*Сохранение дынных*/
 
 
-function saved(evt) {
-  evt.preventDefault();
-  nameProfile.textContent = nameInput.value;
-  jobProfile.textContent = jobInput.value;
-  closed();
-}
-formElement.addEventListener('submit', saved);
 
